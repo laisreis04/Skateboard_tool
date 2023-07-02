32327ea9ee9f4f8ac867a65935e6b6b093586248
Lais Reis <laisreis04@gmail.com>
2023-07-02 13:04:00 +0100

    Page_2
    Novas alterações, página 2, mas JS (que essa porra não tá funcionando).

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Button_index
 var buttom = document.getElementById("botaohp");
 
 buttom.addEventListener("mouseover", function (){
@@ -7,3 +8,16 @@ buttom.addEventListener("mouseover", function (){
 buttom.addEventListener("mouseout", function() {
     buttom.classList.remove("pop-out");
 });
+
+//Imagens_Page_2
+var buttom = document.getElementsByClassName("frame-button");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("mouseover", function() {
+      this.classList.add("pop-out");
+    });
+  
+    buttons[i].addEventListener("mouseout", function() {
+      this.classList.remove("pop-out");
+    });
+  }
