e159332288d8f56239931cc101d68640973597cc
Lais Reis <laisreis04@gmail.com>
2023-07-13 19:39:11 +0100

    update

--- index.js
+++ index.js
@@ -12,13 +12,3 @@ button.addEventListener("mouseleave", function() {
 
 //action for page 2_part 2
 
-
-var button = document.getElementById("instagram");
-
-button.addEventListener("mouseenter", function (){
-    button.classList.add("pop-out");
-});
-
-button.addEventListener("mouseleave", function() {
-    button.classList.remove("pop-out");
-});
