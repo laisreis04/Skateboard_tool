38252573236c5b937270cba2cc756307a710b2fc
Lais Reis <laisreis04@gmail.com>
2023-07-13 19:11:35 +0100

    updates

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ button.addEventListener("mouseleave", function() {
 
 //action for page 2_part 2
 
-var buttom = document.getElementsByClassName("frames");
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("mouseenter", function() {
-      this.classList.add("pop-out");
-    });
-  
-    buttons[i].addEventListener("mouseleave", function() {
-      this.classList.remove("pop-out");
-    });
-  }
+var button = document.getElementById("instagram");
+
+button.addEventListener("mouseenter", function (){
+    button.classList.add("pop-out");
+});
+
+button.addEventListener("mouseleave", function() {
+    button.classList.remove("pop-out");
+});
